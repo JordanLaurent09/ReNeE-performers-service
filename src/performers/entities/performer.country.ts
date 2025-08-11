0a9e90f8fc280d997058a1c4c00f41e5fd7da1df
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2025-08-11 17:47:55 +0300

    enum Country added

---
+++ src/performers/entities/performer.country.ts
@@ -0,0 +1,13 @@
+export enum Country {
+    Russia = 'Russia',
+    Germany = 'Germany',
+    Britain = 'Britain',
+    USA = 'USA',
+    Japan = 'Japan',
+    Italy = 'Italy',
+    Turkey = 'Turkey', 
+    Egypt = 'Egypt',
+    Australia = 'Australia',
+    Sweden = 'Sweden',
+    Norway = 'Norway'
+}
